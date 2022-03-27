89d65807fd71bb457db9893241bc68e578e7c2dc
Valdir <valdirferreira2006@gmail.com>
2022-03-26 22:59:29 -0300

    Inclusão projeto

---
+++ WebMinimalOracle/Config/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebMinimalOracle.Models;
+
+namespace WebMinimalOracle.Config
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options):base(options)
+        {
+           // Database.EnsureCreated();
+        }
+
+        public DbSet<Produto> Produto { get; set; }
+
+
+    }
+}
